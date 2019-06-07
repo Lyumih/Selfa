99fc616154fd369f2a9e5f3957a818c8cf74b2e5
Lyumih <32732179+Lyumih@users.noreply.github.com>
2019-06-07 20:42:00 +0300

    Create: calculator page

---
+++ calculator.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    lvl: 1,
+    lvlMax: 4,
+    power: 2,
+    powerMax: 10,
+    cost: 3,
+    costMax: 4,
+    cooldown: 8,
+    cooldownMax: 3,
+    result: ''
+  },
+  methods: {
+    calc: function() {
+      console.log('calc')
+      this.result = "lvl def: " + (this.lvlMax - this.lvl) + " power: " + this.powerMax / this.power + ". cost: " + this.costMax / this.cost + ". cooldown: " + this.cooldownMax / this.cooldown
+    }
+  }
+})
